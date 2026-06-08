b5ef6b64f53da35658a2a51ce0b936bd123dc894
Dulina Duljaya <ultrasploitofficial@gmail.com>
2026-06-08 21:44:35 +0530

    Added cors for dev and prod envs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{
     App, HttpServer,
     middleware::{Logger, NormalizePath, TrailingSlash},
@@ -16,23 +17,38 @@ mod utils;
 async fn main() -> std::io::Result<()> {
     telemetry::init();
     let config = config::Config::from_env();
+    let dev_mode = config.dev;
 
     tracing::info!("Loading the password database...");
     let passwords = password_db::load_db("database/passwords.bin")?;
     tracing::info!("Loaded {} password hashes", passwords.len());
 
-    let state = state::AppState::from(config, passwords);
+    let state = state::AppState::from(config.clone(), passwords);
     let state = web::Data::new(state);
 
-    tracing::info!("Starting server...");
+    tracing::info!(
+        "Starting server in {} mode...",
+        if dev_mode { "dev" } else { "prod" }
+    );
     let server = HttpServer::new(move || {
+        let cors = if dev_mode {
+            Cors::default()
+                .allow_any_origin()
+                .allow_any_method()
+                .allow_any_header()
+                .max_age(3600)
+        } else {
+            Cors::default()
+        };
+
         App::new()
             .wrap(NormalizePath::new(TrailingSlash::Trim))
             .wrap(Logger::default())
+            .wrap(cors)
             .app_data(state.clone())
             .configure(api::configure)
     })
-    .bind("0.0.0.0:8080")?;
+    .bind(("0.0.0.0", 8080))?;
 
     tracing::info!("Server listening on port 8080");
     server.run().await
